33fd22fbb103a87e7534614ad546e3a7d4f46492
keith <keith.perkins@cnu.edu>
2020-10-01 23:18:20 -0400

    init

---
+++ src/demo.cpp
@@ -0,0 +1,17 @@
+
+
+//lets take a breather and go over what
+//we have learned by doing the following
+
+//lets open a file
+//read the file (parsing out tokens between ,'s
+//chuck each line in a struct that holds first name,last name
+//put each struct in a vector
+//close the file
+//sort the vector
+//write it back to another file
+
+int main() {
+
+	return 0;
+}

430193d57f11ddb8faa95ba31216514538079192
keith <keith@keith-VirtualBox>
2020-10-07 12:55:11 -0400

    327-1

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,17 +1,57 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+const int SUCCESS=0;
+const int COULD_NOT_OPEN_FILE =-1;
 
-//lets take a breather and go over what
-//we have learned by doing the following
+const string INPUT_FILE ="input.txt";
+const string OUTPUT_FILE ="output.txt";
+const char SEPERATOR = ' ';
 
-//lets open a file
-//read the file (parsing out tokens between ,'s
-//chuck each line in a struct that holds first name,last name
-//put each struct in a vector
-//close the file
-//sort the vector
-//write it back to another file
+struct person{
+	string first;
+	string last;
+};
 
 int main() {
+	vector<person> mv;
 
-	return 0;
+	//lets open a file
+	ifstream myInputfile;
+	myInputfile.open(INPUT_FILE.c_str());
+
+	//make sure its opened, return failure code if not
+	if (!myInputfile.is_open())
+		return COULD_NOT_OPEN_FILE;
+
+	string line;
+	person mp;
+
+	//use for loop or while loop?
+	//for(int i=0; i< WHATGOESHERE?<i++)  #bad choice if you have no idea how many times thru the loop
+	while(!myInputfile.eof())	{
+
+		//get a line line from file
+		getline(myInputfile, line);
+
+		//parsing out tokens between ' 's in that line
+		//chuck each line in a struct that holds first name,last name
+		stringstream ss(line);
+		getline(ss, mp.first,SEPERATOR);
+		getline(ss, mp.last,SEPERATOR);
+
+		//put each struct in a vector
+		mv.push_back(mp);
+	}
+
+	//close the file
+
+	//sort the vector
+
+	//write it back to another file
+
+	return SUCCESS;
 }
